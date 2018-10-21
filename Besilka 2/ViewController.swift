7b751f2478f06f2b1916ef18b1233758fd8b8b93
Tatjana Ocedova <tatjana.z.ilievska@gmail.com>
2018-10-21 20:52:06 +0200

    posledna izmena

---
+++ Besilka 2/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Besilka 2
+//
+//  Created by Tatjana Ocedova on 9/29/18.
+//  Copyright © 2018 Tatjana Ocedova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBAction func novaigraKopce(_ sender: Any) {
+    }
+    
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+    
+}
+
